e59b3c98331e5498951d0df6ffac7e82cb245997
Alex Paklin <alexander.pak@prpl.global>
2021-10-09 11:39:38 +0500

    working with tags repository

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,8 +1,11 @@
+import { TagEntity } from './tag.entity';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TagController } from './tag.controller';
 import { TagService } from './tag.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TagEntity])],
   controllers: [TagController],
   providers: [TagService],
 })
