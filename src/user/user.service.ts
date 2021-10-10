45fe6619c040ba1ddbdf1541e6937050297e6d8f
Alex Paklin <alexander.pak@prpl.global>
2021-10-10 11:25:10 +0500

    generating jwt token

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,11 @@
+import { UserResponseInterface } from './types/userResponse.interface';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { Injectable } from '@nestjs/common';
 import { UserEntity } from '@app/user/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
+import { JWT_SECRET } from '@app/config';
 
 @Injectable()
 export class UserService {
@@ -15,5 +18,25 @@ export class UserService {
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     return await this.userRepository.save(newUser);
+  }
+
+  generateJwt(user: UserEntity): string {
+    return sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      JWT_SECRET,
+    );
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user),
+      },
+    };
   }
 }
